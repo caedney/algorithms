5f807461cf3e05086b943d9d3981329b480c8d42
Craig Edney <caedney@btinternet.com>
2025-09-07 10:04:40 +0100

    feat: add exercise 1.3.33

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_3/Exercise_1_3_33.java
@@ -0,0 +1,59 @@
+package algorithms.Exercises.Exercise_1_3;
+
+/**
+ * Exercise 1.3.33
+ * 
+ * <p>
+ * <i>Deque</i>. A double-ended queue or <i>deque</i> (pronounced "deck") is
+ * like a stack or a queue but supports adding and removing items at both ends.
+ * A deque stores a collection of items and supports the following API:
+ * </p>
+ * {@code public class Deque<Item> implements Iterable<Item>}
+ * <table>
+ * <tbody>
+ * <tr>
+ * <td></td>
+ * <td>{@code Deque()}</td>
+ * <td><i>create an empty deque</i></td>
+ * </tr>
+ * <tr>
+ * <td>{@code boolean}</td>
+ * <td>{@code isEmpty()}</td>
+ * <td><i>is the deque empty?</i></td>
+ * </tr>
+ * <tr>
+ * <td>{@code int}</td>
+ * <td>{@code size()}</td>
+ * <td><i>number of items in the deque</i></td>
+ * </tr>
+ * <tr>
+ * <td>{@code void}</td>
+ * <td>{@code pushLeft(Item item)}</td>
+ * <td><i>add an item to the left end</i></td>
+ * </tr>
+ * <tr>
+ * <td>{@code void}</td>
+ * <td>{@code pushRight(Item item)}</td>
+ * <td><i>add an item to the right end</i></td>
+ * </tr>
+ * <tr>
+ * <td>{@code Item}</td>
+ * <td>{@code popLeft()}</td>
+ * <td><i>remove an item from the left end</i></td>
+ * </tr>
+ * <tr>
+ * <td>{@code Item}</td>
+ * <td>{@code popRight()}</td>
+ * <td><i>remove an item from the right end</i></td>
+ * </tr>
+ * </tbody>
+ * </table>
+ * <p>
+ * Write a class {@code Deque} that uses a doubly-linked list to implement this
+ * API and a class {@code ResizingArrayDeque} that uses a resizing array.
+ * </p>
+ */
+public class Exercise_1_3_33 {
+    public static void main(String[] args) {
+    }
+}
