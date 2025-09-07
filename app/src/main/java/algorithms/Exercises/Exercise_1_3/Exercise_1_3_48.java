d2cb43d69c043debd71fd69cce0a939192a79e5a
Craig Edney <caedney@btinternet.com>
2025-09-07 10:05:53 +0100

    feat: add exercise 1.3.48

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_3/Exercise_1_3_48.java
@@ -0,0 +1,15 @@
+package algorithms.Exercises.Exercise_1_3;
+
+/**
+ * Exercise 1.3.48
+ * 
+ * <p>
+ * <i>Two stacks with a deque</i>. Implement two stacks with a single deque so
+ * that each operation takes a constant number of deque operations (see EXERCISE
+ * 1.3.33).
+ * </p>
+ */
+public class Exercise_1_3_48 {
+    public static void main(String[] args) {
+    }
+}
