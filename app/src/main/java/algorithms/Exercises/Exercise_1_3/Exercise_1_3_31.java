3ba95d2381c4fad1fcbea5fa3fd81972ace6b57d
Craig Edney <caedney@btinternet.com>
2025-09-07 10:04:32 +0100

    feat: add exercise 1.3.31

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_3/Exercise_1_3_31.java
@@ -0,0 +1,18 @@
+package algorithms.Exercises.Exercise_1_3;
+
+/**
+ * Exercise 1.3.31
+ * 
+ * <p>
+ * Implement a nested class {@code DoubleNode} for building doubly-linked lists,
+ * where each node contains a reference to the item preceding it and the item
+ * following it in the list ({@code null} if there is no such item). Then
+ * implement static methods for the following tasks: insert at the beginning,
+ * insert at the end, remove from the beginning, remove from the end, insert
+ * before a given node, insert after a given node, and remove a given node.
+ * </p>
+ */
+public class Exercise_1_3_31 {
+    public static void main(String[] args) {
+    }
+}
