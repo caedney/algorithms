9c12a5923e603b3eb3e7832f94b76249335e3586
Craig Edney <caedney@btinternet.com>
2025-09-07 09:55:30 +0100

    feat: add exercise 1.1.10

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_1/Exercise_1_1_10.java
@@ -0,0 +1,23 @@
+package algorithms.Exercises.Exercise_1_1;
+
+/**
+ * Exercise 1.1.10
+ * 
+ * <p>
+ * What is wrong with the following code fragment?
+ * </p>
+ * 
+ * {@snippet :
+ * int[] a;
+ * for (int i = 0; i < 10; i++)
+ *     a[i] = i * i;
+ * }
+ * 
+ * <p>
+ * <i>Answer</i>: Variable {@code a} might not have been initialized
+ * {@code a[i] = i * i};
+ */
+public class Exercise_1_1_10 {
+    public static void main(String[] args) {
+    }
+}
