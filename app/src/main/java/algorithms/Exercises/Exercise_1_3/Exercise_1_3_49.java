231338c233a275f4adfa838f7caf741dfb2bcb69
Craig Edney <caedney@btinternet.com>
2025-09-07 10:05:57 +0100

    feat: add exercise 1.3.49

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_3/Exercise_1_3_49.java
@@ -0,0 +1,15 @@
+package algorithms.Exercises.Exercise_1_3;
+
+/**
+ * Exercise 1.3.49
+ * 
+ * <p>
+ * <i>Queue with three stacks</i>. Implement a queue with three stacks so that
+ * each queue operation takes a constant (worst-case) number of stack
+ * operations. <i>Warning</i>: high degree of difficulty.
+ * </p>
+ */
+public class Exercise_1_3_49 {
+    public static void main(String[] args) {
+    }
+}
