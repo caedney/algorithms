64587069f7773e2d56cb5697de0feea2eb4dc5f8
Craig Edney <caedney@btinternet.com>
2025-09-07 10:05:18 +0100

    feat: add exercise 1.3.41

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_3/Exercise_1_3_41.java
@@ -0,0 +1,21 @@
+package algorithms.Exercises.Exercise_1_3;
+
+/**
+ * Exercise 1.3.41
+ * 
+ * <p>
+ * <i>Copy a queue</i>. Create a new constructor so
+ * </p>
+ * {@code Queue<Item> r = new Queue<Item>(q);}
+ * <p>
+ * makes {@code r} a reference to a new and independent copy of the queue
+ * {@code q}. You should be able to push and pop from either {@code q} or
+ * {@code r} without influencing the other. <i>Hint</i>: Delete all of the
+ * elements from {@code q} and add these elements to both {@code q} and
+ * {@code r}.
+ * </p>
+ */
+public class Exercise_1_3_41 {
+    public static void main(String[] args) {
+    }
+}
