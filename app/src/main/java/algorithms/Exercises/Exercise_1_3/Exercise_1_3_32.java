c702bd666eb8e18493ff9db3439f86cd6a349f62
Craig Edney <caedney@btinternet.com>
2025-09-07 10:04:36 +0100

    feat: add exercise 1.3.32

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_3/Exercise_1_3_32.java
@@ -0,0 +1,15 @@
+package algorithms.Exercises.Exercise_1_3;
+
+/**
+ * Exercise 1.3.32
+ * 
+ * <p>
+ * <i>Steque</i>. A stack-ended queue or <i>steque</i> is a data type that
+ * supports <i>push</i>, <i>pop</i>, and <i>enqueue</i>. Articulate an API for
+ * this ADT. Develop a linked-list-based implementation.
+ * </p>
+ */
+public class Exercise_1_3_32 {
+    public static void main(String[] args) {
+    }
+}
