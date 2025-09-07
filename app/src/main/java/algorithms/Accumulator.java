3e0386ef35c4607bea7c13692cd626ac30019c90
Craig Edney <caedney@btinternet.com>
2025-09-07 09:52:50 +0100

    feat: add classes

---
+++ app/src/main/java/algorithms/Accumulator.java
@@ -0,0 +1,32 @@
+package algorithms;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Accumulator {
+    private double total;
+    private int N;
+
+    public void addDataValue(double val) {
+        N++;
+        total += val;
+    }
+
+    public double mean() {
+        return total / N;
+    }
+
+    public String toString() {
+        return "Mean (" + N + " values): " + String.format("%7.5f", mean());
+    }
+
+    public static void main(String[] args) {
+        int T = Integer.parseInt(args[0]);
+        Accumulator a = new Accumulator();
+
+        for (int t = 0; t < T; t++)
+            a.addDataValue(StdRandom.uniformDouble());
+
+        StdOut.println(a);
+    }
+}
---
+++ app/src/main/java/algorithms/Queue.java
@@ -0,0 +1,90 @@
+package algorithms;
+
+import java.util.Iterator;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Queue<Item> implements Iterable<Item> {
+    private Node first;
+    private Node last;
+    private int N;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void enqueue(Item item) {
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+
+        if (isEmpty())
+            first = last;
+        else
+            oldLast.next = last;
+
+        N++;
+    }
+
+    public Item dequeue() {
+        Item item = first.item;
+        first = first.next;
+
+        if (isEmpty())
+            last = null;
+
+        N--;
+
+        return item;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        public void remove() {
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue<String> queue = new Queue<String>();
+
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-"))
+                queue.enqueue(item);
+            else if (!queue.isEmpty())
+                StdOut.print(queue.dequeue() + " ");
+        }
+
+        StdOut.println("(" + queue.size() + " left on queue)");
+
+        for (String str : queue) {
+            StdOut.println(str);
+        }
+    }
+}
