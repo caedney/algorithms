4c14826300e1559c4a171811eff88355d51e8f83
Craig Edney <caedney@btinternet.com>
2025-09-07 09:58:15 +0100

    feat: add exercise 1.1.38

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_1/Exercise_1_1_38.java
@@ -0,0 +1,19 @@
+package algorithms.Exercises.Exercise_1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercise 1.1.38
+ * 
+ * <p>
+ * <i>Binary search versus brute-force search</i>. Write a program
+ * {@code BruteForceSearch} that uses the brute-force search method given on
+ * page 48 and compare its running time on your computer with that of {@code
+ * BinarySearch} for {@code largew.txt} and {@code largeT.txt}.
+ * </p>
+ */
+public class Exercise_1_1_38 {
+    public static void main() {
+        StdOut.println("Exercise 1.1.38");
+    }
+}
