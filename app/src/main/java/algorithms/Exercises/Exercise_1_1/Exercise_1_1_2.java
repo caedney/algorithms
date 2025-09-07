6771232b3af965bdb82661d5c0834b7db01e1443
Craig Edney <caedney@btinternet.com>
2025-09-07 09:53:48 +0100

    feat: add exercise 1.1.2

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_1/Exercise_1_1_2.java
@@ -0,0 +1,26 @@
+package algorithms.Exercises.Exercise_1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercise 1.1.2
+ * 
+ * <p>
+ * Give the type and value of each of the following expressions:
+ * </p>
+ * 
+ * <ol type="a">
+ * <li>(1 + 2.236) / 2</li>
+ * <li>1 + 2 + 3 + 4.0</li>
+ * <li>4.1 >= 4</li>
+ * <li>1 + 2 + "3"</li>
+ * </ol>
+ */
+public class Exercise_1_1_2 {
+    public static void main(String[] args) {
+        StdOut.println((1 + 2.236) / 2); // 1.618
+        StdOut.println(1 + 2 + 3 + 4.0); // 10.0
+        StdOut.println(4.1 >= 4); // true
+        StdOut.println(1 + 2 + "3"); // 33
+    }
+}
