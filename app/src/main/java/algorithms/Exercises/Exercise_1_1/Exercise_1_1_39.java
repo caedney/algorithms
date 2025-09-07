7a46a7fb85216d7d55dbf4e044bb9ab8d3da1e96
Craig Edney <caedney@btinternet.com>
2025-09-07 09:58:20 +0100

    feat: add exercise 1.1.39

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_1/Exercise_1_1_39.java
@@ -0,0 +1,22 @@
+package algorithms.Exercises.Exercise_1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercise 1.1.39
+ * 
+ * <p>
+ * <i>Random matches</i>. Write a {@code BinarySearch} client that takes an
+ * {@code int} value {@code T} as command-line argument and runs 𝑇 trials of
+ * the following experiment for 𝑁=10<sup>3</sup>, 10<sup>4</sup>,
+ * 10<sup>5</sup>, and 10<sup>6</sup>: generate two arrays of 𝑁 randomly
+ * generated positive six-digit {@code int} values, and find the number of
+ * values that appear in both arrays. Print a table giving the average value of
+ * this quantity over the 𝑇 trials for each value of 𝑁.
+ * </p>
+ */
+public class Exercise_1_1_39 {
+    public static void main() {
+        StdOut.println("Exercise 1.1.39");
+    }
+}
