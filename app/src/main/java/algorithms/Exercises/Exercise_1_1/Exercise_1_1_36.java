ddf93bd3a194206da9754f06fb3a599b812e6d8a
Craig Edney <caedney@btinternet.com>
2025-09-07 09:58:05 +0100

    feat: add exercise 1.1.36

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_1/Exercise_1_1_36.java
@@ -0,0 +1,22 @@
+package algorithms.Exercises.Exercise_1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercise 1.1.36
+ * 
+ * <p>
+ * <i>Empirical shuffle check</i>. Run computational experiments to check that
+ * our shuffling code on page 32 works as advertised. Write a program
+ * {@code ShuffleTest} that takes command-line arguments 𝑀 and 𝑁, does 𝑁
+ * shuffles of an array of size 𝑀 that is initialized with {@code a[i] = i}
+ * before each shuffle, and prints an 𝑀-by-𝑀 table such that row {@code i}
+ * gives the number of times {@code i} wound up in position {@code j} for all
+ * {@code j}. All entries in the array should be close to 𝑁/𝑀.
+ * </p>
+ */
+public class Exercise_1_1_36 {
+    public static void main() {
+        StdOut.println("Exercise 1.1.36");
+    }
+}
