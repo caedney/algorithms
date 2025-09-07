5c039d4c97cc2375bb23b839d9f419e1c97837f2
Craig Edney <caedney@btinternet.com>
2025-09-07 10:05:28 +0100

    feat: add exercise 1.3.43

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_3/Exercise_1_3_43.java
@@ -0,0 +1,17 @@
+package algorithms.Exercises.Exercise_1_3;
+
+/**
+ * Exercise 1.3.43
+ * 
+ * <p>
+ * <i>Listing files</i>. A folder is a list of files and folders. Write a
+ * program that takes the name of a folder as a command-line argument and prints
+ * out all of the files contained in that folder, with the contents of each
+ * folder recursively listed (indented) under that folder's name. <i>Hint</i>:
+ * Use a queue, and see {@code java.io.File}.
+ * </p>
+ */
+public class Exercise_1_3_43 {
+    public static void main(String[] args) {
+    }
+}
