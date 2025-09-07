a41729f2cd0f74291b5168c765a408653f9eb9e6
Craig Edney <caedney@btinternet.com>
2025-09-07 10:04:49 +0100

    feat: add exercise 1.3.35

---
+++ app/src/main/java/algorithms/Card.java
@@ -0,0 +1,31 @@
+package algorithms;
+
+public class Card {
+    private final String rank;
+    private final String suit;
+    private static final String[] RANKS = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+    private static final String[] SUITS = { "♣", "♦", "♥", "♠" };
+
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public static Card[] standardDeck() {
+        Card[] deck = new Card[52];
+        int i = 0;
+
+        for (String suit : SUITS) {
+            for (String rank : RANKS) {
+                deck[i++] = new Card(rank, suit);
+            }
+        }
+
+        return deck;
+    }
+
+    @Override
+    public String toString() {
+        return rank + suit;
+    }
+}
