56b279c725c04be4431aa844fc63f08e73172c52
Craig Edney <caedney@btinternet.com>
2025-09-07 10:05:49 +0100

    feat: add exercise 1.3.47

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_3/Exercise_1_3_47.java
@@ -0,0 +1,16 @@
+package algorithms.Exercises.Exercise_1_3;
+
+/**
+ * Exercise 1.3.47
+ * 
+ * <p>
+ * <i>Catenable queues, stacks, or steques</i>. Add an extra operation
+ * <i>catenation</i> that (de-structively) concatenates two queues, stacks, or
+ * steques (see EXERCISE 1.3.32). <i>Hint</i>: Use a circular linked list,
+ * maintaining a pointer to the last item.
+ * </p>
+ */
+public class Exercise_1_3_47 {
+    public static void main(String[] args) {
+    }
+}
