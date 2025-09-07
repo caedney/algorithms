c03c8940f7c6a21706c0baedfe19aee8a82c2cc9
Craig Edney <caedney@btinternet.com>
2025-09-07 09:58:10 +0100

    feat: add exercise 1.1.37

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_1/Exercise_1_1_37.java
@@ -0,0 +1,20 @@
+package algorithms.Exercises.Exercise_1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercise 1.1.37
+ * 
+ * <p>
+ * <i>Bad shuffling</i>. Suppose that you choose a random integer between
+ * {@code 0} and {@code N-1} in our shuffling code instead of one between
+ * {@code i} and {@code N-1}. Show that the resulting order is not equally
+ * likely to be one of the 𝑁! possibilities. Run the test of the previous
+ * exercise for this version.
+ * </p>
+ */
+public class Exercise_1_1_37 {
+    public static void main() {
+        StdOut.println("Exercise 1.1.37");
+    }
+}
