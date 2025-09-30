a832839df0090da7a24531725658e7648cf3f996
Craig Edney <caedney@btinternet.com>
2025-09-30 08:41:21 +0100

    feat: add exercise 1.3.23

--- app/src/main/java/algorithms/Exercises/Exercise_1_1/Exercise_1_1_23.java
+++ app/src/main/java/algorithms/Exercises/Exercise_1_1/Exercise_1_1_23.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
+import algorithms.BinarySearch;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
@@ -18,25 +19,6 @@ import edu.princeton.cs.algs4.StdOut;
  * </p>
  */
 public class Exercise_1_1_23 {
-    private class BinarySearch {
-        public static int rank(int key, int[] array) {
-            int lo = 0;
-            int hi = array.length - 1;
-
-            while (lo <= hi) {
-                int mid = lo + (hi - lo) / 2;
-                if (key < array[mid])
-                    hi = mid - 1;
-                else if (key > array[mid])
-                    lo = mid + 1;
-                else
-                    return mid;
-            }
-
-            return -1;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/data/algs4/tinyT.txt"));
         In in = new In("src/data/algs4/tinyW.txt");
