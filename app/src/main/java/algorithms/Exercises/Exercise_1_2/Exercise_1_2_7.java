ac088b017493ad911054a7dfd17705faf698a84c
Craig Edney <caedney@btinternet.com>
2025-10-01 08:55:50 +0100

    feat: add exercise 1.2.7

--- app/src/main/java/algorithms/Exercises/Exercise_1_2/Exercise_1_2_7.java
+++ app/src/main/java/algorithms/Exercises/Exercise_1_2/Exercise_1_2_7.java
@@ -12,16 +12,19 @@ import edu.princeton.cs.algs4.StdOut;
  * {@snippet :
  * public static String mystery(String s) {
  *     int N = s.length();
+ * 
  *     if (N <= 1)
  *         return s;
+ * 
  *     String a = s.substring(0, N / 2);
  *     String b = s.substring(N / 2, N);
+ * 
  *     return mystery(b) + mystery(a);
  * }
  * }
  */
 public class Exercise_1_2_7 {
-    private static String mystery(String s) {
+    public static String mystery(String s) {
         int N = s.length();
 
         if (N <= 1)
@@ -35,6 +38,12 @@ public class Exercise_1_2_7 {
 
     public static void main(String[] args) {
         String reversed = mystery("The quick brown fox");
+        StdOut.println("The quick brown fox");
+        StdOut.println("( brown fox)(The quick)");
+        StdOut.println("((n fox)( brow))((quick)(The ))");
+        StdOut.println("(((fox)(n ))((row)( b)))(((ick)(qu))((e )(Th)))");
+        StdOut.println("((((ox)(f))(( )(n)))(((ow)(r))((b)( ))))((((ck)(i))((u)(q)))((( )(e))((h)(T))))");
+        StdOut.println("(((((x)(o))(f))(( )(n)))((((w)(o))(r))((b)( ))))(((((k)(c))(i))((u)(q)))((( )(e))((h)(T))))");
         StdOut.println(reversed);
     }
 }
