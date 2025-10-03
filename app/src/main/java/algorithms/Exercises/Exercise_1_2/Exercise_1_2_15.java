ac79aad5cbbc1ea5060cb9e4f4315ac46f500b0d
Craig Edney <caedney@btinternet.com>
2025-10-03 07:12:30 +0100

    feat: add exercise 1.2.15

--- app/src/main/java/algorithms/Exercises/Exercise_1_2/Exercise_1_2_15.java
+++ app/src/main/java/algorithms/Exercises/Exercise_1_2/Exercise_1_2_15.java
@@ -2,7 +2,7 @@ package algorithms.Exercises.Exercise_1_2;
 
 import java.util.Arrays;
 
-import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
@@ -36,14 +36,19 @@ import edu.princeton.cs.algs4.StdOut;
  * </p>
  */
 public class Exercise_1_2_15 {
-    public static void main(String[] args) {
-        String input = StdIn.readAll();
+    public static int[] readInts(String name) {
+        In in = new In(name);
+        String input = in.readAll();
         String[] words = input.split("\\s+");
         int[] ints = new int[words.length];
 
         for (int i = 0; i < words.length; i++)
             ints[i] = Integer.parseInt(words[i]);
 
-        StdOut.println(Arrays.toString(ints));
+        return ints;
+    }
+
+    public static void main(String[] args) {
+        StdOut.println(Arrays.toString(readInts("src/data/algs4/tinyW.txt")));
     }
 }
