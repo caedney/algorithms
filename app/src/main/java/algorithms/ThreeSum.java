f5279d5e942a306ff8de72c0df5c8ab38feacf56
Craig Edney <caedney@btinternet.com>
2025-10-31 08:57:33 +0000

    feat: add ThreeSum class

---
+++ app/src/main/java/algorithms/ThreeSum.java
@@ -0,0 +1,27 @@
+package algorithms;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class ThreeSum {
+    public static int count(int[] a) {
+        int N = a.length;
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++)
+            for (int j = i + 1; j < N; j++)
+                for (int k = j + 1; k < N; k++)
+                    if (a[i] + a[j] + a[k] == 0)
+                        cnt++;
+
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new In("src/data/algs4/8Kints.txt").readAllInts();
+        Stopwatch timer = new Stopwatch();
+        int cnt = count(a);
+        double time = timer.elapsedTime();
+        StdOut.println(cnt + " in " + time + " seconds");
+    }
+}
