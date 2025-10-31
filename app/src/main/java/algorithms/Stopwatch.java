27bbafb6cf387397e2801c0a2f0403bab91c3bc5
Craig Edney <caedney@btinternet.com>
2025-10-31 08:57:49 +0000

    feat: add Stopwatch class

---
+++ app/src/main/java/algorithms/Stopwatch.java
@@ -0,0 +1,14 @@
+package algorithms;
+
+public class Stopwatch {
+    private final long start;
+
+    public Stopwatch() {
+        start = System.nanoTime();
+    }
+
+    public double elapsedTime() {
+        long now = System.nanoTime();
+        return (now - start) / 1.0e9;
+    }
+}
