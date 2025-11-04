1fac53816c3a85727acdfd61f1042134f5992c03
Craig Edney <caedney@btinternet.com>
2025-11-04 06:28:01 +0000

    feat: add TwoSumFast class

---
+++ app/src/main/java/algorithms/TwoSumFast.java
@@ -0,0 +1,28 @@
+package algorithms;
+
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class TwoSumFast {
+    public static int count(int[] a) {
+        Arrays.sort(a);
+        int N = a.length;
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++)
+            if (BinarySearch.rank(-a[i], a) > i)
+                cnt++;
+
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new In("src/data/algs4/32Kints.txt").readAllInts();
+        Stopwatch timer = new Stopwatch();
+        int cnt = count(a);
+        double time = timer.elapsedTime();
+        StdOut.println(cnt + " in " + time + " seconds");
+    }
+}
