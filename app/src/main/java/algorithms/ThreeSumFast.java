7a55ad2e6ce244394df6e5ea8c0e7feb71be18f5
Craig Edney <caedney@btinternet.com>
2025-11-04 06:28:13 +0000

    feat: add ThreeSumFast class

---
+++ app/src/main/java/algorithms/ThreeSumFast.java
@@ -0,0 +1,29 @@
+package algorithms;
+
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class ThreeSumFast {
+    public static int count(int[] a) {
+        Arrays.sort(a);
+        int N = a.length;
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++)
+            for (int j = i + 1; j < N; j++)
+                if (BinarySearch.rank(-a[i] - a[j], a) > j)
+                    cnt++;
+
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] ints = new In(args[0]).readAllInts();
+        Stopwatch timer = new Stopwatch();
+        int cnt = count(ints);
+        double time = timer.elapsedTime();
+        StdOut.println(cnt + " in " + time + " seconds");
+    }
+}
