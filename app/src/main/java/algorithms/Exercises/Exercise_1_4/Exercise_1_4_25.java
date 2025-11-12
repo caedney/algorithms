a9fcac6bcf54f5aa7032e57d5b1e69c1bb191be7
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:31 +0000

    feat: add exercise 1.4.25

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_25.java
@@ -0,0 +1,18 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.25
+ * 
+ * <p>
+ * <i>Throwing two eggs from a building</i>. Consider the previous question, but
+ * now suppose you only have two eggs, and your cost model is the number of
+ * throws. Devise a strategy to determine <i>F</i> such that the number of
+ * throws is at most 2√<i>N</i>, then find a way to reduce the cost to
+ * ~<i>c</i>√<i>F</i>. This is analogous to a situation where search hits (egg
+ * intact) are much cheaper than misses (egg broken).
+ * </p>
+ */
+public class Exercise_1_4_25 {
+    public static void main(String[] args) {
+    }
+}
