4c6c75ccf94b5de09cd71ca2021edd0470a48ea5
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:31 +0000

    feat: add exercise 1.4.26

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_26.java
@@ -0,0 +1,18 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.26
+ * 
+ * <p>
+ * <i>3-collinearity</i>. Suppose that you have an algorithm that takes as input
+ * <i>N</i> distinct points in the plane and can return the number of triples
+ * that fall on the same line. Show that you can use this algorithm to solve the
+ * 3-sum problem. <i>Strong hint</i>: Use algebra to show that (<i>a</i>,
+ * <i>a</i>³), (<i>b</i>, <i>b</i>³), and (<i>c</i>, <i>c</i>³) are collinear if
+ * and only if <i>a</i> + <i>b</i> + <i>c</i> = 0.
+ * </p>
+ */
+public class Exercise_1_4_26 {
+    public static void main(String[] args) {
+    }
+}
