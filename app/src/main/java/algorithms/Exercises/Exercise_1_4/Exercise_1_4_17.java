5afe7268cc8e07f8768c008f44b5cfde523d7956
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:30 +0000

    feat: add exercise 1.4.17

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_17.java
@@ -0,0 +1,17 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.17
+ * 
+ * <p>
+ * <i>Farthest pair (in one dimension)</i>. Write a program that, given an array
+ * <code>a[]</code> of <i>N</i> <code>double</code> values, finds a <i>farthest
+ * pair</i>: two values whose difference is no smaller than the the difference
+ * of any other pair (in absolute value). The running time of your program
+ * should be linear in the worst case.
+ * </p>
+ */
+public class Exercise_1_4_17 {
+    public static void main(String[] args) {
+    }
+}
