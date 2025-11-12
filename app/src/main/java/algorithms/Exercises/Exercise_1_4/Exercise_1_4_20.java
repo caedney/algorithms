6da3865f79500807882283307e1e1ed9a04c2fe0
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:31 +0000

    feat: add exercise 1.4.20

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_20.java
@@ -0,0 +1,17 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.20
+ * 
+ * <p>
+ * <i>Bitonic search</i>. An array is <i>bitonic</i> if it is comprised of an
+ * increasing sequence of integers followed immediately by a decreasing sequence
+ * of integers. Write a program that, given a bitonic array of <i>N</i> distinct
+ * <code>int</code> values, determines whether a given integer is in the array.
+ * Your program sould use ~3lg <i>N</i> compares in the worst case.
+ * </p>
+ */
+public class Exercise_1_4_20 {
+    public static void main(String[] args) {
+    }
+}
