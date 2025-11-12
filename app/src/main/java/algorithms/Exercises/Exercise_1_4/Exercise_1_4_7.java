48b9fdc8c8192ccafb285af6b31851072fa6ffd5
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:29 +0000

    feat: add exercise 1.4.7

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_7.java
@@ -0,0 +1,14 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.7
+ * 
+ * <p>
+ * Analyze <code>ThreeSum</code> under a cost model that counts arithmetic
+ * operations (and comparisons) involving the input numbers.
+ * </p>
+ */
+public class Exercise_1_4_7 {
+    public static void main(String[] args) {
+    }
+}
