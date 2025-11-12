2d1abcea1d9be96e1945060af4c033f7bcf051d7
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:32 +0000

    feat: add exercise 1.4.29

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_29.java
@@ -0,0 +1,15 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.29
+ * 
+ * <p>
+ * <i>Steque with two stacks</i>. Implement a steque with two stacks so that
+ * each steque operation (see EXERCISE 1.3.32) takes a constant amortized number
+ * of stack operations.
+ * </p>
+ */
+public class Exercise_1_4_29 {
+    public static void main(String[] args) {
+    }
+}
