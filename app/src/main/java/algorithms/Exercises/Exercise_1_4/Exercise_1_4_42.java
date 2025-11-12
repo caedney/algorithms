4132aeb65b558ceca8c56490fc3ed24d44dec61e
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:33 +0000

    feat: add exercise 1.4.42

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_42.java
@@ -0,0 +1,17 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.42
+ * 
+ * <p>
+ * <i>Problem sizes</i>. Estimate the size of the largest value of <i>P</i> for
+ * which you can run <code>TwoSumFast</code>, <code>TwoSum</code>,
+ * <code>ThreeSumFast</code>, and <code>ThreeSum</code> on your computer to
+ * solve the problems for a file of 2<i>ᵖ</i> thousand numbers. Use
+ * <code>DoublingRatio</code> to do so.
+ * </p>
+ */
+public class Exercise_1_4_42 {
+    public static void main(String[] args) {
+    }
+}
