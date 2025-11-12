318f32cc492cd6f16548f4ee4689712893a50af4
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:33 +0000

    feat: add exercise 1.4.45

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_45.java
@@ -0,0 +1,20 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/******************************************************************************
+ * Exercise 1.4.45
+ * 
+ * <p>
+ * <i>Coupon collector problem</i>. Generating random integers as in the
+ * previous exercise, run experiments to validate the hypothesis that the number
+ * of integers generated before all possible values are generated is
+ * ~𝑁𝐻<sub>𝑁</sub>.
+ * </p>
+ * 
+ * <pre>
+ * ./gradlew run -PmainClass=Exercises.Exercise_1_4.Exercise_1_4_45
+ * </pre>
+ ******************************************************************************/
+public class Exercise_1_4_45 {
+    public static void main(String[] args) {
+    }
+}
