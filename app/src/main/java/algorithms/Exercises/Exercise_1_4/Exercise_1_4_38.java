3736f683bcd8e70d4f3cd55635237c7e5577f836
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:33 +0000

    feat: add exercise 1.4.38

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_38.java
@@ -0,0 +1,31 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.38
+ * 
+ * <p>
+ * <i>Naive 3-sum implementation.</i> Run experiments to evaluate the following
+ * im. plementation of the inner loop of <code>ThreeSum</code>:
+ * </p>
+ * 
+ * <pre>
+ * <code>
+ *for (int i = 0; i < N; i++)
+ *    for(int j = 0; j < N; j++)
+ *        for (int k = 0; k < N; k++)
+ *            if (i < j && j < k)
+ *                if(a[i] + a[j] + a[k] == 0)
+ *                    cnt++;
+ * </code>
+ * </pre>
+ * 
+ * <p>
+ * Do so by developing a version of <code>DoublingTest</code> that computes the
+ * ratio of the running times of this program and <code>ThreeSum</code>.
+ * </p>
+ * </p>
+ */
+public class Exercise_1_4_38 {
+    public static void main(String[] args) {
+    }
+}
