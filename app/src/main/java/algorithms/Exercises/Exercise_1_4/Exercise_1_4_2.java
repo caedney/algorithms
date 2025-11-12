b496869e87f377cff682c97ce00b883557a4cfa5
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:29 +0000

    feat: add exercise 1.4.2

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_2.java
@@ -0,0 +1,14 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.2
+ * 
+ * <p>
+ * Modify <code>ThreeSum</code> to work properly even when the <code>int</code>
+ * values are so large that adding two of them might cause overflow.
+ * </p>
+ */
+public class Exercise_1_4_2 {
+    public static void main(String[] args) {
+    }
+}
