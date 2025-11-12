308ec395f4f6a86174e44fca47c6ec680201f299
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:33 +0000

    feat: add exercise 1.4.45

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_45.java
@@ -0,0 +1,16 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.45
+ * 
+ * <p>
+ * <i>Coupon collector problem</i>. Generating random integers as in the
+ * previous exercise, run experiments to validate the hypothesis that the number
+ * of integers generated before all possible values are generated is
+ * ~<i>N</i>H<i><sub>N</sub></i>.
+ * </p>
+ */
+public class Exercise_1_4_45 {
+    public static void main(String[] args) {
+    }
+}
