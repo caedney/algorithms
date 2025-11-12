f7ee1956192fd7fcb0d3ab08d3129436f654adae
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:28 +0000

    feat: add exercise 1.4.1

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_1.java
@@ -0,0 +1,15 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.1
+ * 
+ * <p>
+ * Show that the number of different triples that can be chosen from <i>N</i>
+ * items is precisely <i>N</i>(<i>N</i>-1)(<i>N</i>-2)/6. <i>Hint</i>: Use
+ * mathematical induction or a counting argument.
+ * </p>
+ */
+public class Exercise_1_4_1 {
+    public static void main(String[] args) {
+    }
+}
