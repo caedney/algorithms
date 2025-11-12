5ac2b1999de582da426cc51ddc9b1b769f8c8694
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:31 +0000

    feat: add exercise 1.4.23

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_23.java
@@ -0,0 +1,17 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.23
+ * 
+ * <p>
+ * <i>Binary search for a fraction</i>. Devise a method that uses a logarithmic
+ * number of queries of the form <i>Is the number less than x</i>? to find a
+ * rational number <i>p</i>/<i>q</i> such that 0 < <i>p</i> < <i>q</i> <
+ * <i>N</i>. <i>Hint</i>: Two fractions with denominators less than <i>N</i>
+ * cannot differ by more than 1/<i>N</i>².
+ * </p>
+ */
+public class Exercise_1_4_23 {
+    public static void main(String[] args) {
+    }
+}
