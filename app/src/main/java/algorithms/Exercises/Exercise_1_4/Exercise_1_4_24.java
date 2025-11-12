a255fae329d38346818231dff7fd49ead9503146
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:31 +0000

    feat: add exercise 1.4.24

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_24.java
@@ -0,0 +1,21 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/******************************************************************************
+ * Exercise 1.4.24
+ * 
+ * <p>
+ * <i>Throwing eggs from a building</i>. Suppose that you have an 𝑁-story
+ * building and plenty of eggs. Suppose also that an egg is broken if it is
+ * thrown off floor 𝐹 or higher, and intact otherwise. First, devise a strategy
+ * to determine the value of 𝐹 such that the number of broken eggs is ~lg 𝑁
+ * when using ~lg 𝑁 throws, then find a way to reduce the cost to ~2 lg 𝐹.
+ * </p>
+ * 
+ * <pre>
+ * ./gradlew run -PmainClass=Exercises.Exercise_1_4.Exercise_1_4_24
+ * </pre>
+ ******************************************************************************/
+public class Exercise_1_4_24 {
+    public static void main(String[] args) {
+    }
+}
