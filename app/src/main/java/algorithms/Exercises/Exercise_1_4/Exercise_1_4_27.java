58a2320ffdf8c5ed5f91df912e59910ea69656ce
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:31 +0000

    feat: add exercise 1.4.27

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_27.java
@@ -0,0 +1,17 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.27
+ * 
+ * <p>
+ * <i>Queue with two stacks</i>. Implement a queue with two stacks so that each
+ * queue operation takes a constant amortized number of stack operations.
+ * <i>Hint</i>: If you push elements onto a stack and then pop them all, they
+ * appear in reverse order. If you repeat this process, they're now back in
+ * order.
+ * </p>
+ */
+public class Exercise_1_4_27 {
+    public static void main(String[] args) {
+    }
+}
