fe72ca1df6cd06f23fff65b915d921d49b84ac95
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:33 +0000

    feat: add exercise 1.4.44

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_44.java
@@ -0,0 +1,21 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/******************************************************************************
+ * Exercise 1.4.44
+ * 
+ * <p>
+ * <i>Birthday problem</i>. Write a program that takes an integer 𝑁 from the
+ * command line and uses <code>StRandom.uniform()</code> to generate a random
+ * sequence of integers between 0 and 𝑁 - 1. Run experiments to validate the
+ * hypothesis that the number of integers generated before the first repeated
+ * value is found is ~√π𝑁/2.
+ * </p>
+ * 
+ * <pre>
+ * ./gradlew run -PmainClass=Exercises.Exercise_1_4.Exercise_1_4_44
+ * </pre>
+ ******************************************************************************/
+public class Exercise_1_4_44 {
+    public static void main(String[] args) {
+    }
+}
