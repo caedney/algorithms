8bd343a7ba8fa01dd548ff5939d7eafe6559e0ef
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:30 +0000

    feat: add exercise 1.4.11

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_11.java
@@ -0,0 +1,15 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.11
+ * 
+ * <p>
+ * Add an instance method <code>howMany()</code> to <code>StaticSETofInts</code>
+ * (page 99) that finds the number of occurrences of a given key in time
+ * proportional to log <i>N</i> in the worst case.
+ * </p>
+ */
+public class Exercise_1_4_11 {
+    public static void main(String[] args) {
+    }
+}
