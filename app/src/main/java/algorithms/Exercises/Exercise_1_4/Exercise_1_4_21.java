2be9ad26e803ba53056b5df9a9aa2a5b06035099
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:31 +0000

    feat: add exercise 1.4.21

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_21.java
@@ -0,0 +1,17 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.21
+ * 
+ * <p>
+ * <i>Binary search on distinct values</i>. Develop an implementation of binary
+ * search for <code>StaticSETofInts</code> (see page 98) where the running time
+ * of <code>contains()</code> is guaranteed to be ~lg <i>R</i>, where <i>R</i>
+ * is the number of different integers in the array given as argument to the
+ * constructor.
+ * </p>
+ */
+public class Exercise_1_4_21 {
+    public static void main(String[] args) {
+    }
+}
