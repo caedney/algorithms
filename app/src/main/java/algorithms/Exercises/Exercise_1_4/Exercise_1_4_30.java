d0cbbc24d979eb09b2159cc389431544bcc2fabd
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:32 +0000

    feat: add exercise 1.4.30

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_30.java
@@ -0,0 +1,15 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.30
+ * 
+ * <p>
+ * <i>Deque with a stack and a steque</i>. Implement a deque with a stack and a
+ * steque (see EXERCISE 1.3.32) so that each deque operation takes a constant
+ * amortized number of stack and steque operations.
+ * </p>
+ */
+public class Exercise_1_4_30 {
+    public static void main(String[] args) {
+    }
+}
