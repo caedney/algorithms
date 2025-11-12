ba8fe4b95753131b0f5cbdf687ea03c2b62c0651
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:30 +0000

    feat: add exercise 1.4.16

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_16.java
@@ -0,0 +1,17 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.16
+ * 
+ * <p>
+ * <i>Closest pair (in one dimension)</i>. Write a program that, given an array
+ * <code>a[]</code> of <i>N</i> <code>double</code> values, finds a <i>closest
+ * pair</i>: two values whose difference is no greater than the the difference
+ * of any other pair (in absolute value). The running time of your program
+ * should be linearithmic in the worst case.
+ * </p>
+ */
+public class Exercise_1_4_16 {
+    public static void main(String[] args) {
+    }
+}
