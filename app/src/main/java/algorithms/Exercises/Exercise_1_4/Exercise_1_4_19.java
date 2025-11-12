1b7e6a0672764bd05caef1f7267708471f565b52
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:31 +0000

    feat: add exercise 1.4.19

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_19.java
@@ -0,0 +1,19 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.19
+ * 
+ * <p>
+ * <i>Local minimum of a matrix.</i> Given an <i>N</i>-by-<i>N</i> array
+ * <code>a[]</code> of <i>N</i>² distinct integers, design an algorithm that
+ * runs in time proportional to <i>N</i> to find a <i>local minimum</i>: a pair
+ * of indices <code>i</code> and <code>j</code> such that
+ * <code>a[i][j] < a[i+1][j], a[i][j] < a[i][j+1], a[i][j] < a[i-1][j]</code>,
+ * and <code>a[i][j] < a[i][j-1]</code>. The running time of your program should
+ * be proportional to <i>N</i> in the worst case.
+ * </p>
+ */
+public class Exercise_1_4_19 {
+    public static void main(String[] args) {
+    }
+}
