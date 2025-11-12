41e2a9a2c1e9fef3daa07808b35776b96393abdd
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:30 +0000

    feat: add exercise 1.4.13

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_13.java
@@ -0,0 +1,25 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.13
+ * 
+ * <p>
+ * Using the assumptions developed in the text, give the amount of memory needed
+ * to represent an object of each of the following types:
+ * </p>
+ * 
+ * <ol>
+ * <li><code>Accumulator</code></li>
+ * <li><code>Transaction</code></li>
+ * <li><code>FixedCapacityStackOfStrings</code> with capacity <i>C</i> and
+ * <i>N</i> entries</li>
+ * <li><code>Point2D</code></li>
+ * <li><code>Interval1D</code></li>
+ * <li><code>Interval2D</code></li>
+ * <li><code>Double</code></li>
+ * </ol>
+ */
+public class Exercise_1_4_13 {
+    public static void main(String[] args) {
+    }
+}
