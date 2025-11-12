a993dedb1427eb1084f657feaa98b47e16173207
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:32 +0000

    feat: add exercise 1.4.32

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_32.java
@@ -0,0 +1,19 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/******************************************************************************
+ * Exercise 1.4.32
+ * 
+ * <p>
+ * <i>Amortized analysis</i>. Prove that, starting from an empty stack, the
+ * number of array accesses used by any sequence of 𝑀 operations in the
+ * resizing array implementation of <code>Stack</code> is proportional to 𝑀.
+ * </p>
+ * 
+ * <pre>
+ * ./gradlew run -PmainClass=Exercises.Exercise_1_4.Exercise_1_4_32
+ * </pre>
+ ******************************************************************************/
+public class Exercise_1_4_32 {
+    public static void main(String[] args) {
+    }
+}
