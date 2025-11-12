aa5112ff543562b4cd37b2c0fb5f682f20c64820
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:29 +0000

    feat: add exercise 1.4.5

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_5.java
@@ -0,0 +1,23 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.5
+ * 
+ * <p>
+ * Give tilde approximations for the following quantities:
+ * </p>
+ * 
+ * <ol>
+ * <li><i>N</i> + 1</li>
+ * <li>1 + 1/<i>N</i></li>
+ * <li>(1 + 1/<i>N</i>)(1 + 2/<i>N</i>)</li>
+ * <li>2<i>N</i>³ - 15<i>N</i>² + <i>N</i></li>
+ * <li>lg(2<i>N</i>) / lg <i>N</i></li>
+ * <li>lg(<i>N</i>² + 1) / lg <i>N</i></li>
+ * <li><i>N</i>¹⁰⁰ / 2ᴺ</li>
+ * </ol>
+ */
+public class Exercise_1_4_5 {
+    public static void main(String[] args) {
+    }
+}
