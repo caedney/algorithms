5f58db9cffbbc6f9e3a44e4c1d38edcedb984041
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:30 +0000

    feat: add exercise 1.4.14

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_14.java
@@ -0,0 +1,13 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.14
+ * 
+ * <p>
+ * <i>4-sum</i>. Develop an algorithm for the <i>4-sum</i> problem.
+ * </p>
+ */
+public class Exercise_1_4_14 {
+    public static void main(String[] args) {
+    }
+}
