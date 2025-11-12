acbb516baae32a57e8b415b73c9ef18686a29522
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:32 +0000

    feat: add exercise 1.4.34

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_34.java
@@ -0,0 +1,23 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/******************************************************************************
+ * Exercise 1.4.34
+ * 
+ * <p>
+ * <i>Hot or cold</i>. Your goal is to guess a secret integer between 1 and 𝑁.
+ * You repeatedly guess integers between 1 and 𝑁. After each guess you learn if
+ * your guess equals the secret integer (and the game stops). Otherwise, you
+ * learn if the guess is hotter (closer to) or colder (farther from the secret
+ * number than your previous guess. Design an algorithm that finds the secret
+ * number in at most ~2 lg 𝑁 guesses. Then design an algorithm that finds the
+ * secret number in at most ~1 lg 𝑁 guesses.
+ * </p>
+ * 
+ * <pre>
+ * ./gradlew run -PmainClass=Exercises.Exercise_1_4.Exercise_1_4_34
+ * </pre>
+ ******************************************************************************/
+public class Exercise_1_4_34 {
+    public static void main(String[] args) {
+    }
+}
