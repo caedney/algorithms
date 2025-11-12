6e09fd1d872cb197f283771ffb5afa801b520b94
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:29 +0000

    feat: add exercise 1.4.10

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_10.java
@@ -0,0 +1,15 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.10
+ * 
+ * <p>
+ * Modify binary search so that it always returns the element with the smallest
+ * index that matches the search element (and still guarantees logarithmic
+ * running time).
+ * </p>
+ */
+public class Exercise_1_4_10 {
+    public static void main(String[] args) {
+    }
+}
