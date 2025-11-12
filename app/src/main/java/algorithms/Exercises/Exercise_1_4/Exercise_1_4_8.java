ebb08c81ed9448b9c5dd8a3288daabdbaa984b10
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:29 +0000

    feat: add exercise 1.4.8

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_8.java
@@ -0,0 +1,15 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.8
+ * 
+ * <p>
+ * Write a program to determine the number pairs of values in an input file that
+ * are equal. If your first try is quadratic, think again and use
+ * <code>Arrays.sort()</code> to develop a linearithmic solution.
+ * </p>
+ */
+public class Exercise_1_4_8 {
+    public static void main(String[] args) {
+    }
+}
