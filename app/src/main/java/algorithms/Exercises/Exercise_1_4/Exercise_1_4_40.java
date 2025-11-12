362b99d014f3e8fa0b5e34f5707a74170029817e
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:33 +0000

    feat: add exercise 1.4.40

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_40.java
@@ -0,0 +1,18 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.40
+ * 
+ * <p>
+ * <i>3-sum for random values</i>. Formulate and validate a hypothesis
+ * describing the number of triples of <i>N</i> random <code>int</code> values
+ * that sum to 0. If you are skilled in mathematical analysis, develop an
+ * appropriate mathematical model for this problem, where the values are
+ * uniformly distributed between -<i>M</i> and <i>M</i>, where <i>M</i> is not
+ * small.
+ * </p>
+ */
+public class Exercise_1_4_40 {
+    public static void main(String[] args) {
+    }
+}
