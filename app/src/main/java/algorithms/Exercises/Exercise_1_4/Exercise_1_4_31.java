661bc387f5f2b2e30acde3f726ee33b048cba40e
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:32 +0000

    feat: add exercise 1.4.31

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_31.java
@@ -0,0 +1,14 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.31
+ * 
+ * <p>
+ * <i>Deque with three stacks</i>. Implement a deque with three stacks so that
+ * each deque operation takes a constant amortized number of stack operations.
+ * </p>
+ */
+public class Exercise_1_4_31 {
+    public static void main(String[] args) {
+    }
+}
