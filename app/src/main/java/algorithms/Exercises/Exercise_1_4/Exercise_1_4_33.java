196cca80e061f8e2f35851f5570a2a44590bb3e9
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:32 +0000

    feat: add exercise 1.4.33

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_33.java
@@ -0,0 +1,18 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.33
+ * 
+ * <p>
+ * <i>Memory requirements on a 32-bit machine</i>. Give the memory requirements
+ * for <code>Integer</code>, <code>Date</code>, <code>Counter</code>,
+ * <code>int[]</code>, <code>double[]</code>, <code>double[][]</code>,
+ * <code>String</code>, <code>Node</code>, and <code>Stack</code> (linked-list
+ * representation) for a 32-bit machine. Assume that references are 4 bytes,
+ * object overhead is 8 bytes, and padding is to a multiple of 4 bytes.
+ * </p>
+ */
+public class Exercise_1_4_33 {
+    public static void main(String[] args) {
+    }
+}
