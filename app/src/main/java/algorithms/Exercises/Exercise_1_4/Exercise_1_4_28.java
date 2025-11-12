6e50dc62c395596da03f6da1d82ed5a7768b3a00
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:31 +0000

    feat: add exercise 1.4.28

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_28.java
@@ -0,0 +1,17 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.28
+ * 
+ * <p>
+ * <i>Stack with a queue</i>. Implement a stack with a single queue so that each
+ * stack operations takes a linear number of queue operations. <i>Hint</i>: To
+ * delete an item, get all of the elements on the queue one at a time, and put
+ * them at the end, except for the last one which you should delete and return.
+ * (This solution is admittedly very inefficient.)
+ * </p>
+ */
+public class Exercise_1_4_28 {
+    public static void main(String[] args) {
+    }
+}
