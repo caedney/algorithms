27a91c7b136498b6ed8284811c98c0be600b0577
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:30 +0000

    feat: add exercise 1.4.15

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_15.java
@@ -0,0 +1,17 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.15
+ * 
+ * <p>
+ * <i>Faster 3-sum</i>. As a warmup, develop an implementation
+ * <code>TwoSumFaster</code> that uses a <i>linear</i> algorithm to count the
+ * pairs that sum to zero after the array is sorted (instead of the
+ * binary-search-based linearithmic algorithm). Then apply a similar idea to
+ * develop a quadratic algorithm for the 3-sum problem.
+ * </p>
+ */
+public class Exercise_1_4_15 {
+    public static void main(String[] args) {
+    }
+}
