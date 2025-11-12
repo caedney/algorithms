9f86152233021d472e17ee95190d1374b92e3d29
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:32 +0000

    feat: add exercise 1.4.37

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_37.java
@@ -0,0 +1,18 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.37
+ * 
+ * <p>
+ * <i>Autoboxing performance penalty</i>. Run experiments to determine the
+ * performance penalty on your machine for using autoboxing and auto-unboxing.
+ * Develop an implementation <code>FixedCapacityStackOfInts</code> and use a
+ * client such as <code>DoublingRatio</code> to compare its performance with the
+ * generic {@code FixedCapacityStack<Integer>}, for a large number of
+ * <code>push()</code> and <code>pop()</code> operations.
+ * </p>
+ */
+public class Exercise_1_4_37 {
+    public static void main(String[] args) {
+    }
+}
