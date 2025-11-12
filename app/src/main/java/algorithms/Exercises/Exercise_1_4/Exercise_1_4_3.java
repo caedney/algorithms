caaf8229deeed5223c64e43355944a63dbc5f747
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:29 +0000

    feat: add exercise 1.4.3

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_3.java
@@ -0,0 +1,15 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.3
+ * 
+ * <p>
+ * Modify <code>DoublingTest</code> to use <code>StdDraw</code> to produce plots
+ * like the standard and log-log plots in the text, rescaling as necessary so
+ * that the plot always fills a substantial portion of the window.
+ * </p>
+ */
+public class Exercise_1_4_3 {
+    public static void main(String[] args) {
+    }
+}
