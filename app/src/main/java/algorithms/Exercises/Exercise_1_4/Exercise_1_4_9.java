f256cfe7717d69c76daf00ec696aad9e26c8df38
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:29 +0000

    feat: add exercise 1.4.9

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_9.java
@@ -0,0 +1,15 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.9
+ * 
+ * <p>
+ * Give a formula to predict the running time of a program for a problem of size
+ * <i>N</i> when doubling experiments have shown that the doubling factor is
+ * 2<i>ᵇ</i> and the running time for problems of size <i>N</i>₀ is <i>T</i>.
+ * </p>
+ */
+public class Exercise_1_4_9 {
+    public static void main(String[] args) {
+    }
+}
