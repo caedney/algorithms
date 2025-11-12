d37fb53878aea4bd8d5194c082b39ee45eb922ac
Craig Edney <caedney@btinternet.com>
2025-11-12 17:41:33 +0000

    feat: add exercise 1.4.39

---
+++ app/src/main/java/algorithms/Exercises/Exercise_1_4/Exercise_1_4_39.java
@@ -0,0 +1,17 @@
+package algorithms.Exercises.Exercise_1_4;
+
+/**
+ * Exercise 1.4.39
+ * 
+ * <p>
+ * <i>Improved accuracy for doubling test</i>. Modify <code>DoublingRatio</code>
+ * to take a second command-line argument that specifies the number of calls to
+ * make to <code>timeTrial()</code> for each value of <code>N</code>. Run your
+ * program for 10, 100, and 1,000 trials and comment on the precision of the
+ * results.
+ * </p>
+ */
+public class Exercise_1_4_39 {
+    public static void main(String[] args) {
+    }
+}
